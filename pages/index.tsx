d1106231ed1664795dd38d0e5ff6a877e61d4759
Hayden Pennington <haydenpennington@icloud.com>
2023-03-25 13:43:07 -0500

    Add more copy to site

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,6 @@ const Home: NextPage = () => {
         </a>
         <div className={styles.spacer1}></div>
         <p style={{fontSize: "1.25em", maxWidth: 800, lineHeight: "2em"}}>Center your world around relationships. A graph style file structure with relationships (<span style={{borderRadius: "4px", padding: "4px", backgroundColor: "rgba(128, 128, 128, 0.25)", color: "#FFC962", fontWeight: "bold"}}>labels</span> & <span style={{borderRadius: "4px", padding: "4px", backgroundColor: "rgba(128, 128, 128, 0.25)", color: "#5E81FF", fontWeight: "bold"}}>links</span>), to group together <span style={{borderRadius: "4px", padding: "4px", backgroundColor: "rgba(128, 128, 128, 0.25)", color: "#E173E2", fontWeight: "bold"}}>documents</span></p>
-        
         <div className={styles.spacer1}></div>
         <Image
           src="https://raw.githubusercontent.com/graphnote-io/graphnote/master/Resources/graphnote_screenshot.png" 
@@ -35,6 +34,7 @@ const Home: NextPage = () => {
         />
         <div className={styles.spacer1}></div>
         <h2 className={styles.slogan}>&quot;Augment your memory&quot;</h2>
+        <p style={{fontSize: "1.25em", maxWidth: 800, lineHeight: "2em"}}>A delightful app for note taking and organization where <span style={{borderRadius: "4px", padding: "4px", backgroundColor: "rgba(128, 128, 128, 0.25)", color: "#E173E2", fontWeight: "bold"}}>labels</span> form the navigation hierarchy.</p>
         <a className={styles.comingSoonLink} href="https://github.com/graphnote-app/graphnote">Open Source</a>
         <div className={styles.spacer1}></div>
         <div className={styles.spacer2}></div>
