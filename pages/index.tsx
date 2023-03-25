b1a5bd8073a25cf16c1c6e4775626fae35a671bb
Hayden Pennington <haydenpennington@icloud.com>
2023-03-25 13:28:39 -0500

    Update looks

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,8 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <div className={styles.spacer2}></div>
-        <h1 className={styles.slogan}>Augment your memory</h1>
+        
+        <h1 style={{fontSize: "2.5em"}}>Graphnote</h1>
         <a className={styles.logo} href="https://github.com/graphnote-app">
           <Image
             src="https://raw.githubusercontent.com/graphnote-io/graphnote/master/Resources/graphnote_icon.png"
@@ -23,8 +24,8 @@ const Home: NextPage = () => {
             height={128}
           />
         </a>
-        <h1>Graphnote</h1>
-        <p style={{maxWidth: 800, lineHeight: "2em"}}>Center your world around relationships. A graph style file structure with relationships (<span style={{borderRadius: "4px", padding: "4px", backgroundColor: "rgba(128, 128, 128, 0.25)", color: "#FFC962", fontWeight: "bold"}}>labels</span> & <span style={{borderRadius: "4px", padding: "4px", backgroundColor: "rgba(128, 128, 128, 0.25)", color: "#5E81FF", fontWeight: "bold"}}>links</span>), to group together <span style={{borderRadius: "4px", padding: "4px", backgroundColor: "rgba(128, 128, 128, 0.25)", color: "#E173E2", fontWeight: "bold"}}>documents</span></p>
+        <div className={styles.spacer1}></div>
+        <p style={{fontSize: "1.25em", maxWidth: 800, lineHeight: "2em"}}>Center your world around relationships. A graph style file structure with relationships (<span style={{borderRadius: "4px", padding: "4px", backgroundColor: "rgba(128, 128, 128, 0.25)", color: "#FFC962", fontWeight: "bold"}}>labels</span> & <span style={{borderRadius: "4px", padding: "4px", backgroundColor: "rgba(128, 128, 128, 0.25)", color: "#5E81FF", fontWeight: "bold"}}>links</span>), to group together <span style={{borderRadius: "4px", padding: "4px", backgroundColor: "rgba(128, 128, 128, 0.25)", color: "#E173E2", fontWeight: "bold"}}>documents</span></p>
         
         <div className={styles.spacer1}></div>
         <Image
@@ -32,8 +33,11 @@ const Home: NextPage = () => {
           width={1145}
           height={739}
         />
-        <a className={styles.comingSoonLink} href="https://github.com/graphnote-app/graphnote">Open Source</a>
+        <div className={styles.spacer2}></div>
+        <h2 className={styles.slogan}>"Augment your memory"</h2>
         <div className={styles.spacer1}></div>
+        <a className={styles.comingSoonLink} href="https://github.com/graphnote-app/graphnote">Open Source</a>
+        <div className={styles.spacer2}></div>
         <h3 className={styles.emailHeader}>Be notified by email when the beta is launched</h3>
         <Email />
         <div className={styles.spacer1}></div>
